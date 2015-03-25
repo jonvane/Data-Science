6dc2dd092390a17a70333ce70596928d6d71ffa6
Jonathan Van Erdewyk <jonvane@outlook.com>
2015-03-24 22:35:40 -0500

    one last update tonight

--- 4 - Exploratory Data Analysis/Lattice Plotting System.R
+++ 4 - Exploratory Data Analysis/Lattice Plotting System.R
@@ -35,3 +35,30 @@ p <- xyplot(Ozone~Wind,data=airquality) ## Nothing happens!
 print(p) ## Plot appears
 
 xyplot(Ozone~Wind,data=airquality) ## Auto-printing
+
+## Latice Plotting System (part 2)
+
+## Lattice Panel Functions
+
+set.seed(10)
+x <- rnorm(100)
+f <- rep(0:1,each=50)
+y <- x+f-f*x+rnorm(100,sd=0.5)
+f <- factor(f,labels=c("Group 1","Group 2"))
+xyplot(y~x|f,layout=c(2,1))  ## Plot with 2 panels
+
+## Custom panel function
+
+xyplot(y~x|f,panel=function(x,y,...){
+  panel.xyplot(x,y,...) ## First call the default panel function for 'xyplot'
+  panel.abline(h=median(y),lty=2) ## Add a horizontal line at the median
+})
+
+## Lattice Panel Functions: Regression line
+
+## Custom panel function
+
+xyplot(y~x|f,panel=function(x,y,...){
+  panel.xyplot(x,y,...) ## First call default panel function
+  panel.lmline(x,y,col=2) ## Overlay a simple linear regression line
+})
