047238b381d34ac3dfcad4c2759895cd42b82984
Jonathan Van Erdewyk <jonvane@outlook.com>
2015-03-24 19:29:46 -0500

    more updates

--- 4 - Exploratory Data Analysis/Graphics Devices in R.R
+++ 4 - Exploratory Data Analysis/Graphics Devices in R.R
@@ -1,5 +1,6 @@
 # Graphics Devices in R
 
+# Part 1
 
 # ways to launch the screen device
 
@@ -21,3 +22,24 @@ with(faithful,plot(eruptions,waiting))
 title(main="Old Faithful Geyser data") ## Annotate plot; still nothing on screen
 dev.off() ## Close the PDF file device
 ## Now you can view the file 'myplot.pdf' on your computer
+
+# Part 2
+
+# Multiple Open Graphics Devices
+
+dev.cur() ## currently active graphics device
+dev.set() ## change the active graphics device - number goes in between braces and is associated with the graphics device you want to switch to
+
+# Copying Plots
+
+dev.copy ## copy a plot from one device to another
+dev.copy2pdf ## copy a plot to a PDF file
+
+library(datasets)
+with(faithful,plot(eruptions,waiting))  ## Create plot on screen device
+title(main="Old Faithful Geyser data") ## Add a main title
+dev.copy(png,file="geyserplot.png") ## Copy my plot to a PNG file
+dev.off() ## Don't forget to close the PNG device!
+
+dev.copy2pdf(file="myplot2.pdf")
+dev.off()
